449b913851f81550c53da77852b3b26cd07eb3da
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-03-31 16:04:52 +0200

    > Gitignore updated at 2025-03-31 16:04:52

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './components';
-export { default as Face } from './Face.svelte';
-export * from './utils/constants';
-export type { KawaiiMood, KawaiiProps, KawaiiFaceProps } from './types';
-export declare const getFaceScale: (size: number) => number;
--- dist/index.js
+++
@@ -1,6 +0,0 @@
-export * from './components';
-export { default as Face } from './Face.svelte';
-export * from './utils/constants';
-export const getFaceScale = (size) => {
-    return size / 66;
-};
