2f0cbeacce29734f4704c5545da0bb86a87993ca
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2025-04-01 16:23:21 +0200

    tests: update

--- e2e/pages.test.ts
+++ e2e/pages.test.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test';
 // About page
 test('About page has expected h1', async ({ page }) => {
   await page.goto('/about');
-  expect(await page.textContent('h1')).toBe('Svelte Kawaii');
+  expect(await page.textContent('h1')).toBe('About: Svelte Kawaii');
 });
